4256d1fa7038f808eaf52257db200736520db2d6
Damian Czaja <trojan295@gmail.com>
2019-08-28 23:49:08 +0200

    add termite config

--- .local/bin/configure_multihead.sh
+++ .local/bin/configure_multihead.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-xrandr --setprovideroutputsource Intel NVIDIA-0
+xrandr --setprovideroutputsource modesetting NVIDIA-0
 xrandr --output eDP-1-1 --left-of DP1
 
 $HOME/.local/bin/launch_polybar.sh
