ab2cf69c4021b8372f8301b9967d4c25dfd22f7a
Damian Czaja <trojan295@protonmail.com>
2021-04-27 19:04:10 +0200

    add jetbrains font downloader

--- scripts/jetbrains-fonts.sh
+++ scripts/jetbrains-fonts.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -xe
 
-font_package="JetBrainsMono-1.0.6"
+version="2.1.0"
+zip_name="JetBrainsMono.zip"
 fonts_dir="$HOME/.fonts"
 
-wget https://github.com/JetBrains/JetBrainsMono/releases/download/v1.0.6/${font_package}.zip
-unzip ${font_package}.zip
+wget -O "${zip_name}" "https://github.com/ryanoasis/nerd-fonts/releases/download/v${version}/JetBrainsMono.zip"
+unzip "${zip_name}"
 
-mkdir -p $fonts_dir
-cp ${font_package}/ttf/* ${fonts_dir}
+mkdir -p "${fonts_dir}"
+mv *.ttf "${fonts_dir}"
 
-rm -rf ${font_package}*
-
+rm -rf "${zip_name}"
